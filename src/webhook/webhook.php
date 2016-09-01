0043bc16b8108076042084bf5e4bfc383c84aee7
lavvy <lavashonline@gmail.com>
2016-09-01 01:52:08 +0000

    Add 'src/webhook/webhook.php'

---
+++ src/webhook/webhook.php
@@ -0,0 +1,8 @@
+<?php
+$data = json_decode(file_get_contents('php://input'), true);
+$src = $data["repository"]["clone_url"];
+$reponame = $data["repository"]["name"];
+$cmd ="export REPONAME=".$reponame." && export SRC=".$src." && /var/www/html/webhook/webhook.sh";
+$out = shell_exec($cmd);
+echo $cmd;
+?>
